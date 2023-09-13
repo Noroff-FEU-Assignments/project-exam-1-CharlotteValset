271edd1327b1413a8a39bd7ad0379432683bf584
CharlotteValset <119309299+CharlotteValset@users.noreply.github.com>
2023-09-13 12:44:14 +0200

    js blog fetch

--- js/blog-specific-page.js
+++ js/blog-specific-page.js
@@ -1,6 +1,8 @@
-import { baseApiUrl, endpointApiUrl } from "./variables.js";
+import { baseApiUrl } from "./variables.js";
 import { getBlogPosts } from "./variables.js";
 import { createMessage } from "./errorMessage.js";
+
+const blogDetailsUrl = "/posts?acf_format=standard";
 
 const blogPostContainer = document.querySelector(".blog-post_container");
 const errorMessage = createMessage("error");
@@ -9,15 +11,15 @@ const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
 
-const json = await getBlogPosts(`${baseApiUrl}${endpointApiUrl}/${id}`);
+const jsonSpecific = await getBlogPosts(`${baseApiUrl}${blogDetailsUrl}/${id}`);
 
 async function fetchBlogSpecificImage() {
   try {
     blogPostContainer.innerHTML = "";
 
     const blogPostImage = document.createElement("img");
-    blogPostImage.setAttribute("alt", `${json.acf.images.alt}`);
-    blogPostImage.src = json.acf.images.url;
+    blogPostImage.setAttribute("alt", `${jsonSpecific[0].acf.images.alt}`);
+    blogPostImage.src = jsonSpecific[0].acf.images.url;
     blogPostImage.className = "blog-post_image";
     blogPostContainer.appendChild(blogPostImage);
   } catch (error) {
